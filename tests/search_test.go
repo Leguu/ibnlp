93121e536d19c359286a89b21c6f6a57a50b1941
Leguu <33807061+Leguu@users.noreply.github.com>
2023-03-10 07:47:19 -0500

    Add a test

--- tests/search_test.go
+++ tests/search_test.go
@@ -13,7 +13,7 @@ var searchProvider search.PythonEncodingSearchProvider = search.PythonEncodingSe
 func containsResult(results []search.SearchResult, matchs ...string) bool {
 	for _, result := range results {
 		for _, match := range matchs {
-			if strings.Contains(result.Match, match) {
+			if strings.Contains(strings.ToLower(result.Match), strings.ToLower(match)) {
 				return true
 			}
 		}
@@ -43,7 +43,7 @@ func testQueries(t *testing.T, queries []string, expectedResults []string) {
 	if len(failedQueries) > 0 {
 		log.Println("Failed to find expected results in queries: ", failedQueries)
 
-		if len(failedQueries) > len(queries)/2 {
+		if len(failedQueries) >= len(queries)/2 {
 			t.Fatal("Too many failed queries")
 		}
 	}
@@ -57,15 +57,22 @@ func TestSearchWordCount(t *testing.T) {
 }
 
 func TestSearchPurpose(t *testing.T) {
-	expectedResults := []string{"demonstrate application skills knowledge", "pursue"}
 	queries := []string{"What is the purpose of the internal assessment?", "What is the purpose of the IA?", "Internal assessment purpose", "IA purpose"}
+	expectedResults := []string{"demonstrate application skills knowledge", "pursue"}
+
+	testQueries(t, queries, expectedResults)
+}
+
+func TestSearchCriteria(t *testing.T) {
+	queries := []string{"What are the internal assessment criteria?", "What are the IA criteria?", "Internal assessment criteria", "IA criteria"}
+	expectedResults := []string{"diagrams", "terminology", "application", "analysis", "key concept", "evaluation"}
 
 	testQueries(t, queries, expectedResults)
 }
 
 func TestSustainableDevelopment(t *testing.T) {
-	expectedResults := []string{"poverty", "reduced inequalities", "decent work economic growth", "innovation infrastructure", "responsible consumption production"}
 	queries := []string{"What is sustainable development?", "What is sustainable development in economics?", "What are the sustainable development goals"}
+	expectedResults := []string{"poverty", "reduced inequalities", "decent work economic growth", "innovation infrastructure", "responsible consumption production"}
 
 	testQueries(t, queries, expectedResults)
 }
